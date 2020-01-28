42dde5fdb007e91218d2a9c433c50291debd1d5a
Jonathan Huynh <jonhuynh05@gmail.com>
2020-01-27 20:23:50 -0800

    backend hits

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser")
 const session = require("express-session")
 const fetch = require("node-fetch")
 const PORT = process.env.PORT || 8000
+const id = process.env.API_ID
 const key = process.env.API_KEY
 const bcrypt = require("bcryptjs");
 const User = require("./models/Users")
@@ -24,6 +25,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use("/user", userController)
 
+app.get("/api/v1/", async (req, res) => {
+    try{
+        console.log("this hits")
+        res.send("this is the backend")
+    }
+    catch(err){
+        res.send(err)
+        console.log(err)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}.`)
--- src/Home/index.js
+++ src/Home/index.js
@@ -4,6 +4,8 @@ import {withRouter} from "react-router-dom"
 class Home extends Component{
     async componentDidMount (){
         console.log("this is mounted")
+        const backend = await fetch("/api/v1/")
+        console.log(backend, "got it from the back")
     }
     render(){
         return(
