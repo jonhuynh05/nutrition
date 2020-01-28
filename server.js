4c519a0733e4a2f0ce9da5f34ce60e9b913934e3
Jonathan Huynh <jonhuynh05@gmail.com>
2020-01-27 19:36:28 -0800

    install packages

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require("express")
+const path = require("path")
+const app = express()
+const methodOverride = require("method-override")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const fetch = require("node-fetch")
+const PORT = process.env.PORT || 8000
+const key = process.env.LOL_API_KEY
+const bcrypt = require("bcryptjs");
+console.log(key, "this is the key")
+
+require("./config/db")
+
+app.use(express.static(path.join(__dirname, "build")))
+app.use(session({
+    secret: "little secrets",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(methodOverride("_method"));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}.`)
+})
