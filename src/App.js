c36c6b56684d41d823877482e3cdb782c400551e
Jonathan Huynh <jonhuynh05@gmail.com>
2020-02-18 16:18:01 -0800

    move state to app level

--- src/App.js
+++ src/App.js
@@ -5,11 +5,81 @@ import Food from "./Food"
 
 
 class App extends Component {
+    state = {
+      query: "",
+      foodName: "",
+      foodId: "",
+      foodImg: "",
+      dropdown: [],
+      error: ""
+  }
+
+  getData = async () => {
+      console.log("this hits")
+      this.setState({
+          error: "",
+          foodImg: "",
+      })
+      const dataQuery = await(await fetch(`/api/v1/${this.state.query}`)).json()
+      console.log(dataQuery, "data from back")
+      if (dataQuery === "No results."){
+          this.setState({
+              error: dataQuery
+          })
+      }
+      else{
+          this.setState({
+              foodName: dataQuery.results.food_name,
+              foodId: dataQuery.results.tag_id,
+              foodImg: dataQuery.results.photo.thumb,
+              dropdown: dataQuery.dropdown
+          })
+      }
+  }
+
+  handleChange = (e) => {
+      this.setState({
+          [e.currentTarget.name]: e.currentTarget.value
+      })
+      this.handleSearchResults()
+  }
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+      this.getData()
+  }
+
+  handleSearchResults = async () => {
+      const dataQuery = await(await fetch(`/api/v1/${this.state.query}`)).json()
+      console.log(dataQuery, "data from back")
+      if (dataQuery === "No results."){
+          this.setState({
+              error: dataQuery
+          })
+      }
+      else if (this.state.query === ""){
+          this.setState({
+              foodName: "",
+              foodId: "",
+              foodImg: "",
+              dropdown: []
+          })
+      }
+      else{
+          this.setState({
+              foodName: dataQuery.results.food_name,
+              foodId: dataQuery.results.tag_id,
+              foodImg: dataQuery.results.photo.thumb,
+              dropdown: dataQuery.dropdown
+          })
+      }
+  }
+
   render(){
     return (
       <div>
         <Switch>
-          <Route exact path="/" render={() =><Home/>}/>
+          <Route exact path="/" render={() =><Home dropdown={this.state.dropdown} error={this.state.error} query={this.state.query} getData={this.getData} handleChange={this.handleChange} handleSubmit={this.handleSubmit} handleSearchResults={this.handleSearchResults}/>}/>
           <Route exact path="/:query" render={() =><Food/>}/>
         </Switch>
       </div>
--- src/Home/index.js
+++ src/Home/index.js
@@ -4,79 +4,9 @@ import "./home.css"
 
 class Home extends Component{
 
-    state = {
-        query: "",
-        foodName: "",
-        foodId: "",
-        foodImg: "",
-        dropdown: [],
-        error: ""
-    }
-
-    getData = async () => {
-        console.log("this hits")
-        this.setState({
-            error: "",
-            foodImg: "",
-        })
-        const dataQuery = await(await fetch(`/api/v1/${this.state.query}`)).json()
-        console.log(dataQuery, "data from back")
-        if (dataQuery === "No results."){
-            this.setState({
-                error: dataQuery
-            })
-        }
-        else{
-            this.setState({
-                foodName: dataQuery.results.food_name,
-                foodId: dataQuery.results.tag_id,
-                foodImg: dataQuery.results.photo.thumb,
-                dropdown: dataQuery.dropdown
-            })
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            [e.currentTarget.name]: e.currentTarget.value
-        })
-        this.handleSearchResults()
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.getData()
-    }
-
-    handleSearchResults = async () => {
-        const dataQuery = await(await fetch(`/api/v1/${this.state.query}`)).json()
-        console.log(dataQuery, "data from back")
-        if (dataQuery === "No results."){
-            this.setState({
-                error: dataQuery
-            })
-        }
-        else if (this.state.query === ""){
-            this.setState({
-                foodName: "",
-                foodId: "",
-                foodImg: "",
-                dropdown: []
-            })
-        }
-        else{
-            this.setState({
-                foodName: dataQuery.results.food_name,
-                foodId: dataQuery.results.tag_id,
-                foodImg: dataQuery.results.photo.thumb,
-                dropdown: dataQuery.dropdown
-            })
-        }
-    }
-
     render(){
         const dropdown =
-            this.state.dropdown.map((query, i) => {
+            this.props.dropdown.map((query, i) => {
                 return(
                     <div key={i} className="search-option">
                         <Link className="search-links" to={`/${query}`}>{query}</Link>
@@ -85,15 +15,15 @@ class Home extends Component{
             })
         return(
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input placeholder="Search Food" type="text" name="query" value={this.state.query} onChange={this.handleChange}/>
+                <form onSubmit={this.props.handleSubmit}>
+                    <input placeholder="Search Food" type="text" name="query" value={this.props.query} onChange={this.props.handleChange}/>
                     <button type="submit">Search</button>
                 </form>
                 <div>
                     {dropdown}
                 </div>
                 {
-                    this.state.error === ""
+                    this.props.error === ""
                     ?
                     null
                     :
