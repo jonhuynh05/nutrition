f99e2af7ae17a5393f310bffc030de2126795e18
Jonathan Huynh <jonhuynh05@gmail.com>
2020-02-18 16:37:46 -0800

    pull in query from url

--- src/Food/index.js
+++ src/Food/index.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import {withRouter} from "react-router-dom"
 
 class Food extends Component {
+    async componentDidMount() {
+        console.log(this.props.match.params.query)
+    }
+
     render(){
         return(
             <div>
                 this is the query page
+                {this.props.match.params.query}
             </div>
         )
     }
