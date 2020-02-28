42cce9b448d2a2efe6351056b992933e960390e6
Jonathan Huynh <jonhuynh05@gmail.com>
2020-02-28 14:36:38 -0800

    create db entry

--- controller/user.js
+++ controller/user.js
@@ -5,7 +5,25 @@ const bcrypt = require("bcryptjs")
 
 router.post("/register", async (req, res) => {
     try{
-        const checkUser = null
+        const foundEmail = await User.findOne({
+            email: req.body.email
+        })
+        if(foundEmail){
+            res.json({
+                message: "Email already exists."
+            })
+        }
+        else{
+            const userDbEntry = {}
+            const password = req.body.password
+            const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+            userDbEntry.firstName = req.body.firstName
+            userDbEntry.lastName = req.body.lastName
+            userDbEntry.username = req.body.username
+            userDbEntry.email = req.body.email
+            userDbEntry.password = hashPassword
+            console.log(userDbEntry)
+        }
     }
     catch(err){
         res.send(err)
